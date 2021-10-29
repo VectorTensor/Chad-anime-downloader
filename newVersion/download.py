8595e6ca071e46b329d27558c39b79494e9c348b
VectorTensor <comppro54@gmail.com>
2021-10-29 19:29:50 +0545

    core code completed

--- newVersion/download.py
+++ newVersion/download.py
@@ -1,18 +1,32 @@
 import os 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-PATH="C:\edgeDriver\msedgedriver.exe"
+import time
+PATH="C:\edgeDriver\msedgedriver.exe"# Enter the path of the web driver
 driver = webdriver.Edge(PATH)
 driver.get('https://gogoanime.pe/login.html')
 form=driver.find_element(By.CLASS_NAME,'form-login')
-inp= driver.find_element(By.XPATH,"//input[@type='email']").send_keys('prayashthapa15@yahoo.com')
+inp= driver.find_element(By.XPATH,"//input[@type='email']").send_keys('prayashthapa15@yahoo.com')# login credentials
 pas = driver.find_element(By.XPATH,"//input[@type='password']").send_keys('party@me')
 but= driver.find_element(By.XPATH,"//button[@type='submit']").click()
-#inp.send_keys('prayash')
+#inp.2end_keys('prayash')
+a= 1#first episode
+b=11#last episode
+b=b+1
+counter=1
+for i in range(a,b):
+    x=str(i)
+    driver.get('https://gogoanime.pe/natsume-yuujinchou-roku-episode-'+x)#Link of the anime
+    time.sleep(5) 
+    download_bar= driver.find_element(By.CLASS_NAME,'cf-download')
+    download= download_bar.find_element(By.TAG_NAME,'a')
+    download.click()
+    time.sleep(5)
+    
+    if counter % 3 ==0 :
+        time.sleep(300)
+    counter += 1
 
-
-driver.get('https://gogoanime.pe/toushinki-gs-frame-episode-3')
-driver.implicitly_wait(5)
 #download = driver.find_element(By.CLASS_NAME,'dowload')
 #link= download.find_element(By.TAG_NAME,'a').click() 
 #print("praysh dudue"+ inp[1].text)
