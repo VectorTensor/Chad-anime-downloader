9b681763fc39c1aef7c8f8889a67cc106a3324ab
VectorTensor <comppro54@gmail.com>
2021-11-02 15:46:35 +0545

    final version

--- newVersion/download.py
+++ newVersion/download.py
@@ -6,17 +6,18 @@ PATH="C:\edgeDriver\msedgedriver.exe"# Enter the path of the web driver
 driver = webdriver.Edge(PATH)
 driver.get('https://gogoanime.pe/login.html')
 form=driver.find_element(By.CLASS_NAME,'form-login')
-inp= driver.find_element(By.XPATH,"//input[@type='email']").send_keys('prayashthapa15@yahoo.com')# login credentials
-pas = driver.find_element(By.XPATH,"//input[@type='password']").send_keys('party@me')
+inp= driver.find_element(By.XPATH,"//input[@type='email']").send_keys('')# login credentials
+pas = driver.find_element(By.XPATH,"//input[@type='password']").send_keys('')
 but= driver.find_element(By.XPATH,"//button[@type='submit']").click()
 #inp.2end_keys('prayash')
-a= 1#first episode
-b=11#last episode
+a=20 #first episode
+b=27#last episode
 b=b+1
 counter=1
+url='https://gogoanime.pe/k-on-2-episode-'
 for i in range(a,b):
     x=str(i)
-    driver.get('https://gogoanime.pe/natsume-yuujinchou-roku-episode-'+x)#Link of the anime
+    driver.get(url+x)#Link of the anime
     time.sleep(5) 
     download_bar= driver.find_element(By.CLASS_NAME,'cf-download')
     download= download_bar.find_element(By.TAG_NAME,'a')
